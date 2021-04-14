ac47468d64799db95e759034f96969ecf0f81cfd
uadson <uadsoncastro@gmail.com>
2021-04-14 20:56:06 -0300

    conf data

--- hello_word/settings.py
+++ hello_word/settings.py
@@ -75,14 +75,10 @@ WSGI_APPLICATION = 'hello_word.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-	'default': {
-	'ENGINE': 'django.db.backends.mysql',
-	'NAME': 'db_dj_hello',
-	'USER': 'devuf',
-	'PASSWORD': 'Udn975Cf!@#09',
-	'HOST': 'localhost',
-	'PORT': '3306',
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
